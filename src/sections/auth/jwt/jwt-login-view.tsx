884a476bb8b7b068708ad710413f208283fe72fd
raulmn00 <raul.mariaci@gmail.com>
2024-07-10 21:42:48 -0300

    feat: add login form

--- src/sections/auth/jwt/jwt-login-view.tsx
+++ src/sections/auth/jwt/jwt-login-view.tsx
@@ -1,14 +1,64 @@
+'use client';
+
+import { useState } from 'react';
+
 export default function JwtLoginView() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log({ email, password });
+  };
+
   return (
     <>
-      <h1>Pagina de Login 12345</h1>
-      <form>
-        <input name="email" id="email" title="email" type="email" />
-        <input name="password" id="password" title="password" type="password" />
-        <button type="submit" id="button-submit">
-          Enviar
-        </button>
-      </form>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <div className="bg-white shadow-md rounded-lg px-8 pb-12 pt-16">
+          <h1 className="text-2xl font-bold text-gray-800">Login</h1>
+
+          <form onSubmit={handleSubmit}>
+            <div className="mt-6">
+              <label
+                htmlFor="email"
+                className="block text-gray-700 font-medium"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+                className="mt-1 block w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+
+            <div className="mt-6">
+              <label
+                htmlFor="password"
+                className="block text-gray-700 font-medium"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+                className="mt-1 block w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="mt-8 py-2 px-4 rounded-md bg-blue-500 text-white font-medium hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-600"
+            >
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
